453950ca275ee26d597320db097041329675ce93
Hery Hermawan <hery.hermawan20102@gmail.com>
2024-03-13 14:07:08 +0800

    adding login page

---
+++ android/app/src/main/kotlin/com/example/flutter_siakad_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_siakad_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/common/constants/variables.dart
@@ -0,0 +1,3 @@
+class Variables {
+  static const String baseUrl = 'https://a2ba-114-10-142-200.ngrok-free.app';
+}
